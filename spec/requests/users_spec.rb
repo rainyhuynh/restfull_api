24ccd0176ea4965988d0f11685c8792717ec547a
rainyhuynh <lathoaitan0803@gmail.com>
2022-12-19 11:40:59 +0700

    Done authenticate

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -11,26 +11,26 @@ RSpec.describe 'User API', type: :request do
         context 'when valid request' do
             before { post '/signup', params: valid_attributes.to_json, headers: headers }
             it 'creates a new user' do
-                except(response).to have_http_status(201)
+                expect(response).to have_http_status(201)
             end
 
             it 'returns success message' do
-                except(json['message']).to match(/Account created successfully/)
+                expect(json['message']).to match(/Account created successfully/)
             end
 
             it 'returns an authentication token' do
-                except(json(['auth_token'])).not_to be_nil
+                expect(json['auth_token']).not_to be_nil
             end
         end
 
         context 'when invalid request' do
             before { post '/signup', params: {}, headers: headers}
             it 'does not create a new user' do
-                except(response).to have_http_status(422)
+                expect(response).to have_http_status(422)
             end
 
             it 'returns failure message' do
-                except(json['message']).to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
+                expect(json['message']).to match(/Validation failed: Password can't be blank, Name can't be blank, Email can't be blank, Password digest can't be blank/)
             end
         end
     end
